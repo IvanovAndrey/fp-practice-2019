b814a8d55e8a3e33e20450d722115b03ebb94f33
IvanovAndrey <olifem2012@gmail.com>
2019-11-10 01:45:46 +0300

    Task 1_1 done

--- Task1_1.hs
+++ Task1_1.hs
@@ -7,26 +7,59 @@ module Task1_1 where
 
 import Todo(todo)
 
+data Operator = Plus | Minus | Multiply deriving (Show, Eq)
+
 data Term = IntConstant{ intValue :: Int }           -- числовая константа
             | Variable{ varName :: String }          -- переменная
-            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
+            | BinaryTerm{ lhv :: Term, op::Operator, rhv :: Term } -- бинарная операция
             deriving(Show,Eq)
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) l r = todo
-(|-|) :: Term -> Term -> Term
-(|-|) l r = todo
-(|*|) :: Term -> Term -> Term
-(|*|) l r = todo
+(|+|) l r = BinaryTerm l Plus r
 
--- Заменить переменную `varName` на `replacement`
+(|-|) :: Term -> Term -> Term
+(|-|) l r = BinaryTerm l Minus r
+
+(|*|) :: Term -> Term -> Term
+(|*|) l r = BinaryTerm l Multiply r
+
+infixl 1 |+|
+infixl 1 |-|
+infixl 2 |*|
+
+--Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = todo
+replaceVar varName replacement expression =
+        case expression of
+            Variable v -> if(v == varName) then replacement else expression
+            BinaryTerm lhv op rhv -> BinaryTerm (replaceVar varName  replacement lhv) op (replaceVar varName replacement rhv) 
+            _ -> expression
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate expression = todo
+evaluate expression = 
+    case expression of
+        BinaryTerm lhv op rhv -> 
+            case (evaluate(lhv), op, evaluate(rhv)) of 
+                (IntConstant lhv, Plus, IntConstant rhv) -> IntConstant (lhv + rhv)
+                (IntConstant lhv, Minus, IntConstant rhv) -> IntConstant (lhv - rhv)
+                (IntConstant lhv, Multiply, IntConstant rhv) -> IntConstant (lhv * rhv)
+                _ -> BinaryTerm lhv op rhv
+        _-> expression
+
+
+
+
+
+
+
+
+
+
+
+
+
