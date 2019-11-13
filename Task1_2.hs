0dd2b9fc06de891dd06e0894b4be5d615e22b124
IvanovAndrey <olifem2012@gmail.com>
2019-11-14 02:14:12 +0300

    Task 1_2 done

--- Task1_2.hs
+++ Task1_2.hs
@@ -4,7 +4,7 @@ module Task1_2 where
   Задание 1.2
   Необходимо реализовать четыре любые функции в данном файле
 -}
-
+import Prelude hiding (pow, gcd)
 import Todo(todo)
 
 -- синус числа (формула Тейлора)
@@ -17,7 +17,14 @@ cos x = todo
 
 -- наибольший общий делитель двух чисел
 gcd :: Integer -> Integer -> Integer
-gcd x y = todo
+gcd x y 
+        | x == y = x
+        | x /= y =
+            let 
+              bigger = max x y
+              smaller = min x y
+            in
+                gcd (bigger - smaller) smaller
 
 -- существует ли полный целочисленный квадрат в диапазоне [from, to)?
 doesSquareBetweenExist :: Integer -> Integer -> Bool
@@ -26,16 +33,38 @@ doesSquareBetweenExist from to = todo
 -- является ли дата корректной с учётом количества дней в месяце и
 -- вискокосных годов?
 isDateCorrect :: Integer -> Integer -> Integer -> Bool
-isDateCorrect day month year = todo
-
+isDateCorrect day month year 
+                             | day <= 0 ||day > 31|| month <= 0 || month > 12 || year <=0 =False
+                             | day <= numDays !! fromIntegral month = True
+                             | month == 2 || day <= 28 = True  
+                             | month == 2 || day == 29 = checkFeb year
+                             | otherwise = False
+    where
+        checkFeb year = if mod year 4 == 0 then True else False
+        numDays = [0 ,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
 pow :: Integer -> Integer -> Integer
-pow x y = todo
+pow x y  
+        | y < 0 = error "Incorrect variable y"
+        | y == 0 = 1
+        | x == 1 || x == 0 || y == 1 = x
+        | otherwise = power x x y 
+            where
+                power x mn y   | y == 1 = x
+                             | otherwise = power (x * mn) mn (y - 1)                
 
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
-isPrime x = todo
+isPrime x = 
+            let n = 2 in 
+                if mod x n == 0 then False else isPrime' x $ n+1
+               where
+                   isPrime' x n
+                            | x == 1 = True                   
+                            | x == n = True 
+                            | mod x n == 0 = False
+                            | otherwise = isPrime' x $ n+1
 
 type Point2D = (Double, Double)
 
