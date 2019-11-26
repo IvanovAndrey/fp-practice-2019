2607aeb6ce43f2394c413592da3449e025e93e88
IvanovAndrey <olifem2012@gmail.com>
2019-11-26 14:05:06 +0300

    Task2_2 done

--- Task2_2.hs
+++ Task2_2.hs
@@ -50,12 +50,17 @@ catMaybes = foldr (\i lst -> case i of
 
 -- Диагональ матрицы
 diagonal :: [[a]] -> [a]
-diagonal = todo
-
+diagonal [] = []
+diagonal mat = if  length (head mat) == length (mat) then diagonal' mat 0
+else []
+               
+diagonal' :: [[a]] -> Int -> [a] 
+diagonal' mat n = if length(mat)== 0 then [] else let h = (head (mat) !! n) 
+                  in  h:(diagonal' (drop 1 mat) (n+1))
 
 -- Фильтр для всех элементов, не соответствующих предикату
 filterNot :: (a -> Bool) -> [a] -> [a]
-filterNot = todo
+filterNot p = foldr (\x s -> if p x then s else x:s) []
 
 -- Поиск элемента в списке
 elem :: (Eq a) => a -> [a] -> Bool
@@ -74,4 +79,4 @@ append first second = foldr (\x res -> x:res)  second first
 -- Разбиение списка lst на куски размером n
 -- (последний кусок может быть меньше)
 groups :: [a] -> Integer -> [[a]]
-groups lst n = todo
+groups lst n = unfoldr (\x -> if null x then Nothing else Just (take (fromIntegral n) x, drop (fromIntegral n) x) ) lst
