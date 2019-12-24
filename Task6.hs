11883600a59433caf8675c989420ecd94f29eeac
IvanovAndrey <olifem2012@gmail.com>
2019-12-24 22:35:15 +0300

    Task6 done

--- Task6.hs
+++ Task6.hs
@@ -15,37 +15,76 @@ type Parser a = Parsec String () a
 digit :: Parser Char
 digit = oneOf ['0'..'9']
 
-number :: Parser Integer
-number = read <$> many1 digit
+intNum :: Parser String
+intNum = many1 digit
 
+floatPart :: Parser String
+floatPart = do
+                char '.'
+                res <- many1 digit
+                return ('.':res)
+
+number :: Parser Float
+number = do
+            num <- intNum
+            opt <- option "" floatPart
+            return $ read (num ++ opt)
+            
+negative :: Parser Float
+negative = atom 
+              <|> do
+                   char '-'
+                   spaces
+                   res <- atom
+                   return $ negate res
+                   
+fac :: Float -> Float
+fac 1 = 1
+fac n | n - toEnum(truncate(n)) == 0 = n  * fac (n - 1)
+      | otherwise = error "Not-integer factorial"
+                                        
 applyMany :: a -> [a -> a] -> a
 applyMany x [] = x
 applyMany x (h:t) = applyMany (h x) t
 
-div_ :: Parser (Integer -> Integer -> Integer)
+
+div_ :: Parser (Float -> Float -> Float)
 div_ = do
     char '/'
-    return div
+    return (/)
 
-star :: Parser (Integer -> Integer -> Integer)
+star :: Parser (Float -> Float -> Float)
 star = do
     char '*'
     return (*)
 
-plus :: Parser (Integer -> Integer -> Integer)
+plus :: Parser (Float -> Float -> Float)
 plus = do
     char '+'
     return (+)
 
-minus :: Parser (Integer -> Integer -> Integer)
+minus :: Parser (Float -> Float -> Float)
 minus = do
     char '-'
     return (-)
-
-multiplication :: Parser Integer
+    
+factorial :: Parser (Float -> Float)
+factorial = do
+    char '!'
+    return fac
+    
+factoriation :: Parser Float
+factoriation = do
+    spaces
+    x <- negative
+    spaces
+    ys <- many (factorial)
+    return $ foldl (\ x f -> f x) x ys                
+   
+multiplication :: Parser Float
 multiplication = do
     spaces
-    lhv <- atom
+    lhv <- factoriation
     spaces
     t <- many tail
     return $ applyMany lhv t
@@ -53,11 +92,11 @@ multiplication = do
             do
                 f <- star <|> div_
                 spaces
-                rhv <- atom
+                rhv <- factoriation
                 spaces
                 return (`f` rhv)
 
-addition :: Parser Integer
+addition :: Parser Float
 addition = do
     spaces
     lhv <- multiplication
@@ -72,9 +111,16 @@ addition = do
                 spaces
                 return (`f` rhv)
 
-atom :: Parser Integer
+atom :: Parser Float
 atom = number <|> do
     char '('
     res <- addition
     char ')'
     return res
+
+mainParser :: Parser Float
+mainParser = do
+            spaces
+            p <- addition
+            eof
+            return p
