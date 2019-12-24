32f6c2b67b80933f2b90f5abfa0615cf2b49a83c
IvanovAndrey <olifem2012@gmail.com>
2019-12-24 16:33:08 +0300

    Task5_1 done

--- Task5_1.hs
+++ Task5_1.hs
@@ -34,10 +34,27 @@ list2dlist' left (h: t) =
 
 -- Реализуйте функции индексирования, вставки и удаления элементов
 index :: DList a -> Int -> a
-index = todo
+index DNil _ = error "Empty list"
+index (DCons _ v r) 0 = v
+index (DCons _ v r) i = index r (i - 1)
 
 insertAt :: DList a -> Int -> a -> DList a
-insertAt list index value = todo
+insertAt DNil 0 val = DCons DNil val DNil
+insertAt (DCons l v (DCons _ rv rr)) 0 val = rec where
+    rec = DCons l val newr
+    newr = DCons rec v (DCons newr rv rr)
+insertAt (DCons l v DNil) 0 val = rec where
+    rec = DCons l val r
+    r = DCons rec v DNil
+insertAt (DCons l v r) index val = DCons l v $ insertAt r (index - 1) val
+insertAt DNil _ _ = error "Incorrect index"
 
 removeAt :: DList a -> Int -> DList a
-removeAt list index = todo
+removeAt DNil _ = error "Empty list"
+removeAt (DCons _ _ DNil) 0 = DNil
+removeAt (DCons _ _ DNil) _ = error "Incorrect index"
+removeAt (DCons l _ (DCons _ rv rr)) 0 = DCons l rv rr
+removeAt (DCons l val r) index = DCons l val $ removeAt r (index - 1)
+ 
+b = DNil
+a = list2dlist [1,2,3,4,5]
