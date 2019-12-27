bbb5afc486c3506d0bf9c6666e93fd9ab690735e
IvanovAndrey <olifem2012@gmail.com>
2019-12-27 04:44:25 +0300

    Task5_2 done

--- Task5_2.hs
+++ Task5_2.hs
@@ -11,6 +11,15 @@ data Zipper a = Zipper [a] [a]
 fromList :: [a] -> Zipper a
 fromList lst = Zipper [] lst
 
+toList :: Zipper a -> [a]
+toList (Zipper l r) = concat [l , r]  
+
+instance (Show a) => Show (Zipper a) where
+    show (Zipper l r) = show (reverse l) ++ show r  
+
+instance (Eq a) => Eq (Zipper a) where
+    (==) z1 z2 = toList z1 == toList z2 
+    
 goRight :: Zipper a -> Zipper a
 goRight z@(Zipper _ []) = z
 goRight (Zipper l (rh:rt)) = Zipper (rh:l) rt
@@ -34,11 +43,32 @@ removeLeft (Zipper (_:lt) r) = Zipper lt r
 -- Используя приведённые выше функции, реализуйте функцию конкатенации
 -- вставки подсписка в середину и выделения подсписка
 
-concat :: Zipper a -> Zipper a -> Zipper a
-concat left right = todo
+concatZipper :: Zipper a -> Zipper a -> Zipper a
+concatZipper left right = Zipper (toList left) (toList right)
+
+insertManyAt' :: Int -> Zipper a -> Zipper a -> Zipper a
+insertManyAt' 0 (Zipper lw rw) (Zipper li ri) = (Zipper (concat [li,lw]) (concat [rw,ri]))
+insertManyAt' i what into = insertManyAt' (i - 1) what (goRight into)
 
 insertManyAt :: Int -> Zipper a -> Zipper a -> Zipper a
-insertManyAt index what into = todo
-
+insertManyAt index (Zipper [] []) into = into
+insertManyAt index what (Zipper [] []) = what
+insertManyAt index what (Zipper li ri) = if index < 0 || index > (length(concat [li,ri])) 
+                                         then error "Incorrect Index" 
+                                         else insertManyAt' index what (Zipper [] (concat [li,ri]))
+                                                   
 subZipper :: Int -> Int -> Zipper a -> Zipper a
-subZipper from to input = todo
+subZipper from to (Zipper [] []) = error "Zipper is empty"
+subZipper from to (Zipper li ri) 
+                        | from < 0 || from > (length(concat [li,ri])) = error "Incorrect From"
+                        | to < 0 || to > (length(concat [li,ri])) = error "Incorrect To"
+                        | from > to = error "Error from > to"
+                        | otherwise = Zipper [] $take (to - from + 1) $drop from $concat [li,ri]
+
+-- Берется подсписок [from, to] (включительно)
+
+
+a = [1,2,3,4,5]
+b = [6,7,8,9,10]
+zip1 = Zipper [3,2,1] [4,5,6]
+zip2 = Zipper [2,1] [4,5,6]
